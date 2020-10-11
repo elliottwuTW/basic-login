d9c644ac970da8f135f20092c60ce4298dd83cdf
elliottwuTW <elliott10009@gmail.com>
2020-10-11 22:02:15 +0800

    feat: add cookie-based auth to keep login state

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
 const checkLogin = require('./checkLogin')
+const parseCookies = require('./parseCookies')
 
 const app = express()
 const port = 3000
@@ -17,24 +18,33 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// routes
+// 路由
+// decide whether to go to personal or login page
 app.get('/', (req, res) => {
-  const user = req.query.user
-  if (user) {
+  const cookies = parseCookies(req) // get the cookies object
+  if (cookies && cookies.logged === 'true') {
+    const user = cookies.name
     res.render('index', { user })
   } else {
     res.redirect('/login') // redirect to login page by default
   }
 })
+// log out
+app.post('/', (req, res) => {
+  res.setHeader('Set-Cookie', ['logged=false'])
+  res.redirect('/login')
+})
+// login page
 app.get('/login', (req, res) => {
   res.render('login')
 })
+// check the login information
 app.post('/login', (req, res) => {
   const { account, password } = req.body
-
   const user = checkLogin(account, password)
   if (user) {
-    res.redirect(`/?user=${user.firstName}`)
+    res.setHeader('Set-Cookie', ['logged=true', `name=${user.firstName}`])
+    res.redirect('/')
   } else {
     res.render('login', { loginFail: true })
   }
